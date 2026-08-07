f4fb01f06fa382a73d076526b971a47aed01dbb2
Koustavd18 <kdkdas8@gmail.com>
2026-08-07 11:16:15 +0530

    Binary

--- Binary/add.go
+++ Binary/add.go
@@ -1,3 +1,29 @@
 package Binary
 
-func Add() {}
+import "strconv"
+
+var Binary1 string = "1010"
+var Binary2 string = "1011"
+
+func Add(b1 string, b2 string) string {
+	i := len(b1) - 1
+	j := len(b2) - 1
+	carry := 0
+	result := ""
+
+	for i >= 0 || j >= 0 || carry == 1 {
+
+		if i >= 0 {
+			carry += int(b1[i]) - '0'
+			i--
+		}
+		if j >= 0 {
+			carry += int(b2[j]) - '0'
+			j--
+		}
+		result = strconv.Itoa(carry%2) + result
+		carry /= 2
+	}
+
+	return result
+}
--- main.go
+++ main.go
@@ -3,11 +3,11 @@ package main
 import (
 	"fmt"
 
-	"example.com/DSA/Robot"
+	"example.com/DSA/Binary"
 )
 
 func main() {
 	fmt.Println("Import and Execute")
-	fmt.Println(Robot.Solution(Robot.Moves))
+	fmt.Println(Binary.Add(Binary.Binary1, Binary.Binary2))
 
 }
